afa231ff85bcc9139d55d1b8f6fb3b2efe3bfc77
Erfan Hassanzadeh <erfan.hassanzadeh@spark.net>
2026-03-21 17:41:19 +0100

    feat: add MessageInput component

--- src/chat/Chat.tsx
+++ src/chat/Chat.tsx
@@ -1,10 +1,15 @@
 import { type PropsWithChildren } from 'react'
 import { ChatContainer } from './ChatContainer'
+import { MessageInput } from './MessageInput'
 
 interface OwnProps {}
 
 type Props = PropsWithChildren<OwnProps>
 
 export function Chat(props: Props) {
-  return <ChatContainer>Chat</ChatContainer>
+  return (
+    <ChatContainer>
+      <MessageInput onSendMessage={() => {}} />
+    </ChatContainer>
+  )
 }
---
+++ src/chat/MessageInput.tsx
@@ -0,0 +1,45 @@
+import { useState, type KeyboardEvent, type PropsWithChildren } from 'react'
+import { Textarea } from 'src/shared/ui/textarea'
+import { Button } from 'src/shared/ui/button'
+
+interface OwnProps {
+  onSendMessage: (value: string) => void
+}
+
+type Props = PropsWithChildren<OwnProps>
+
+export function MessageInput(props: Props) {
+  const { onSendMessage } = props
+  const [text, setText] = useState<string>('')
+  const sendMessage = () => {
+    onSendMessage(text)
+    setText('')
+  }
+  const handleKeyDown = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault()
+      sendMessage()
+    }
+  }
+  return (
+    <div className="w-full bg-blue-400">
+      <div className="p-2 flex items-end gap-2 md:w-3xl lg:w-5xl justify-center mx-auto">
+        <Textarea
+          value={text}
+          autoFocus
+          className="p-3 resize-none field-sizing-content max-h-34"
+          placeholder="Message"
+          onChange={(e) => setText(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+        <Button
+          onClick={sendMessage}
+          disabled={!text}
+          className=" w-24 bg-orange-500 text-white"
+        >
+          Send
+        </Button>
+      </div>
+    </div>
+  )
+}
