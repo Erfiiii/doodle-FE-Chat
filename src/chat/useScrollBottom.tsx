727e955429a8de130281c2d9ae4d1f5c9037e4ce
Erfan Hassanzadeh <erfan.hassanzadeh@spark.net>
2026-03-22 17:26:32 +0100

    refactor: prevent scroll to bottom in infinite loading

--- src/chat/useScrollBottom.tsx
+++ src/chat/useScrollBottom.tsx
@@ -4,9 +4,13 @@ import { useMessages } from './MessagesController'
 export const useScrollBottom = () => {
   const { state } = useMessages()
   const scrollBottomRef = useRef<HTMLDivElement>(null)
+  const lastMessageIdRef = useRef<string>('')
   useEffect(() => {
-    if (scrollBottomRef.current) {
+    const lastMessageId = state.messages[state.messages.length - 1]?._id
+
+    if (scrollBottomRef.current && lastMessageId !== lastMessageIdRef.current) {
       scrollBottomRef.current.scrollIntoView()
+      lastMessageIdRef.current = lastMessageId
     }
   }, [state.messages])
 
